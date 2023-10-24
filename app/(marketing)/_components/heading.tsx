07f75d0978f980cf602da7ffc6822cff003c8cee
neothato <139212895+neothato@users.noreply.github.com>
2023-10-24 10:50:06 +0200

    Made changes to the theme and Branding

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -14,12 +14,13 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
+        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Totion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br />
-        better, faster work happens.
-      </h3>
+   Totion is the connected workspace where <br />
+   better, faster work happens. (A Notion Clone by Thato)
+</h3>
+
       {isLoading && (
         <div className="w-full flex items-center justify-center">
           <Spinner size="lg" />
@@ -28,7 +29,7 @@ export const Heading = () => {
       {isAuthenticated && !isLoading && (
         <Button asChild>
           <Link href="/documents">
-            Enter Jotion
+            Enter Totion
             <ArrowRight className="h-4 w-4 ml-2" />
           </Link>
         </Button>
@@ -36,7 +37,7 @@ export const Heading = () => {
       {!isAuthenticated && !isLoading && (
         <SignInButton mode="modal">
           <Button>
-            Get Jotion free
+            Get Totion free
             <ArrowRight className="h-4 w-4 ml-2" />
           </Button>
         </SignInButton>
