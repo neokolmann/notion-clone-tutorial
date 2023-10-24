21cc2451d2bb4df37685b84cc18542b6d0d1c1b7
neothato <139212895+neothato@users.noreply.github.com>
2023-10-24 09:41:16 +0200

    SOme changes to the Authentication

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://selected-grubworm-69.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
