fc4f4eeaa75448b7e0e4733b631eb29b25331301
neothato <139212895+neothato@users.noreply.github.com>
2023-10-24 09:45:52 +0200

    assigned the object to a variable and then exported the variable

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,10 @@
-export default {
+const config = {
   providers: [
     {
       domain: "https://selected-grubworm-69.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
-}
+};
+
+export default config;
